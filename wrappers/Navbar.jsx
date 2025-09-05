dcf42ed2a665f26c0393f5c7ded122fd9fbe3c78
karthijayasangar <karthijayasangar6564@gmail.com>
2025-09-06 03:43:28 +0530

    updated

--- wrappers/Navbar.jsx
+++ wrappers/Navbar.jsx
@@ -32,27 +32,28 @@ const Navbar = () => {
         <img src="/K.jpg" alt="Logo" className="w-10 h-10" />
         <span className="text-xl sm:text-2xl"><span className="text-lime-700">Le</span><span className="text-lime-600">ts</span><span className="text-lime-500">Blo</span><span className="text-lime-400">g</span></span>
       </Link>
-      <div className="sm:hidden" ref={menuRef}>
-        <button className="cursor-pointer" onClick={() => setOpen(pre => !pre)}>
-          {open ? <img src="/close.png" width={13} /> : <img src="/menubar.png" width={20} />}
-        </button>
-        {open && <div className="w-[25%] h-45 absolute right-5 top-11 rounded text-center transition-all duration-300 ease-out 
+      <div className="sm:hidden flex items-center gap-2">
+        <SignedOut>
+          <Link to={'/login'}>
+            <button className="p-1 cursor-pointer text-lime-700 font-bold rounded-full pr-2"> <span className="text-indigo-300">L</span><span className="text-indigo-400">o</span><span className="text-indigo-500">g</span><span className="text-indigo-600">i</span><span className="text-indigo-700">n</span></button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <div ref={menuRef}>
+          <button className="cursor-pointer" onClick={() => setOpen(pre => !pre)}>
+            {open ? <img src="/close.png" width={13} /> : <img src="/menubar.png" width={20} />}
+          </button>
+          {open && <div className="w-[25%] h-45 absolute right-5 top-11 rounded text-center transition-all duration-300 ease-out 
            transform flex flex-col p-2 items-center justify-center text-gray-400 text-sm bg-white shadow" onClick={handleMenuClick}>
-          <Link className="hover:text-lime-600" to={'/'}>Home</Link>
-          <Link className="hover:text-lime-600" to={'/posts'}>Trending</Link>
-          <Link className="hover:text-lime-600" to={'/posts?sort=popular'}>Popular</Link>
-          {user?.username && <Link className="hover:text-lime-600 mb-2" to={`/authorsPage?author=${user?.username}`}>Dashboard</Link>}
-          <NotificationBell />
-          <SignedOut>
-            <Link to={'/login'}>
-              <button className="p-1 cursor-pointer text-lime-700 font-bold bg-lime-100 rounded-full pl-2 pr-2">Login</button>
-            </Link>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-
-        </div>}
+            <Link className="hover:text-lime-600" to={'/'}>Home</Link>
+            <Link className="hover:text-lime-600" to={'/posts'}>Trending</Link>
+            <Link className="hover:text-lime-600" to={'/posts?sort=popular'}>Popular</Link>
+            {user?.username && <Link className="hover:text-lime-600 mb-2" to={`/authorsPage?author=${user?.username}`}>Dashboard</Link>}
+            <NotificationBell />
+          </div>}
+        </div>
       </div>
       <div className="hidden sm:flex items-center gap-6 xl:gap-12 font-semibold text-gray-400 ">
         <Link className="hover:text-lime-600" to={'/'}>Home</Link>
